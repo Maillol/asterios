71c19323de9a98b8961a6dd893a7ad8303d9972f
Vincent Maillol <vincent.maillol@gmail.com>
2018-06-25 08:29:38 +0200

    Remove double setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,53 @@
+import ast
+import pathlib
+import sys
 from setuptools import setup
 
+
+def read_version():
+    """
+    Read the version from `asterios/__version__.py` without import
+    module to avoid side effect.
+    """
+    module_src = (
+        pathlib.Path(__file__).parent / "asterios" / "__version__.py"
+    ).read_text()
+    for statment in ast.parse(module_src).body:
+        if isinstance(statment, ast.Assign):
+            if statment.targets[0].id == "__version__":
+                version = statment.value.s
+                break
+    else:
+        exit(
+            "The `__version__` variable is not defined"
+            " in the `asterios.__version__` module"
+        )
+
+    return version
+
+
 setup(
-    name='asterios',
-    version='0.0.1',
-    description='Asterios escape game server',
-    keywords='Asterios escape game server',
-    author='vincent.maillol@gmail.com',
-    author_email='vincent.maillol@gmail.com',
-    url='https://github.com/Maillol/asterios',
-    license='GPLv3',
-    packages=['asterios'],
+    name="asterios",
+    version=read_version(),
+    description="Asterios escape game server",
+    keywords="Asterios escape game server",
+    author="vincent.maillol@gmail.com",
+    author_email="vincent.maillol@gmail.com",
+    url="https://github.com/Maillol/asterios",
+    license="GPLv3",
+    packages=["asterios"],
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Topic :: Games/Entertainment :: Puzzle Games',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Games/Entertainment :: Puzzle Games",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
     ],
-    python_requires='>=3.5',
+    python_requires=">=3.5",
     install_requires=[
-        'aiohttp>=3.0',
-        'attrs>=17.4',
-        'voluptuous>=0.10',
-        'pyyaml>=3.12',
-        'basicauth>=0.4'
-    ]
+        "aiohttp>=3.0",
+        "attrs>=17.4",
+        "voluptuous>=0.10",
+        "pyyaml>=3.12",
+        "basicauth>=0.4",
+    ],
 )
